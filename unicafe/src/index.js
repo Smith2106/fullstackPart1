246a34b20520f1b1a353ea49b68b76c507b81648
Elijah <elijahs2106@gmail.com>
2019-07-07 23:35:55 -0400

    Finish exercise 1.6

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,6 +1,30 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = ({name, onClick}) => {
+  return (
+    <>
+      <button onClick={onClick}>{name}</button>
+    </>
+  )
+}
+
+const Header = ({title}) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  )
+}
+
+const Count = ({name, count}) => {
+  return (
+    <div>
+      {name} {count}
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -9,7 +33,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header title="Give Feedback" />
+      <Button name="Good" onClick={() => setGood(good + 1)} />
+      <Button name="Neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button name="Bad" onClick={() => setBad(bad + 1)} />
+      <Header title="Statistics" />
+      <Count name="Good" count={good} />
+      <Count name="Neutral" count={neutral} />
+      <Count name="Bad" count={bad} />
     </div>
   )
 }
